42769bd328c99c7c1c9eb9322109d8180a762dc6
Juan <Juan@JUAN-PC>
2016-04-11 14:34:00 -0400

    Add project files.

---
+++ Check in window/Domain/CheckInOut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_in_window.Domain
+{
+    public class CheckInOut
+    {
+        public int Id { get; set; }
+        public byte[] Photo { get; set; }
+        public string Username { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public bool ForceCheckOut { get; set; }
+    }
+}
